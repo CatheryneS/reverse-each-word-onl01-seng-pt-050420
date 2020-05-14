c07d3137ae1637aee45b2d282bd1108812c14f66
Catheryne Small <catheryn.small@gmail.com>
2020-05-14 05:49:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,10 @@
-def reverse_each_word(string)
- str = string.reverse.split
- reversed = []
- str.each { |letter| reversed << "#{letter}" }
- reversed.reverse.join(" ")
-end
-
 #def reverse_each_word(string)
  #str = string.reverse.split
- str.collect
+ #reversed = []
+ #str.each { |letter| reversed << "#{letter}" }
+ #reversed.reverse.join(" ")
+#end
+
+def reverse_each_word(string)
+ string.collect
 end
